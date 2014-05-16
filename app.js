068ee14efdd0142d88dfe68c4a95533fecb9c416
davidpardy <dpardy87@gmail.com>
2014-05-16 02:54:35 -0700

    added a gem and displayed properties

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 (function(){
 var app = angular.module('store', [ ]);
   app.controller('StoreController', function(){
-
+    this.product = gem; //product is a property of this controller
   });
+
+  var gem = {
+    name: 'Ruby',
+    price: 5.00,
+    description: 'real red, real shiny',
+  }
 })();
